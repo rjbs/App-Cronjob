d4e8c4507b9d46dc3290d61e913beca5d22a4a3c
Ricardo SIGNES <rjbs@cpan.org>
2009-03-08 22:57:21 -0400

    tweaks to minimal documentation

--- lib/App/Cronjob.pm
+++ lib/App/Cronjob.pm
@@ -3,4 +3,11 @@ use warnings;
 package App::Cronjob;
 # ABSTRACT: wrap up programs to be run as cron jobs
 
+=head1 SEE INSTEAD
+
+The only useful thing in this distribution, for now, is the L<cronjob|cronjob>
+program.
+
+=cut
+
 1;
